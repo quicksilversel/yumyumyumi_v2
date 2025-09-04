665e2725eb483e46d1c7c72d910682ef77ca1e11
quicksilversel <quicksilversel@gmail.com>
2025-09-04 19:36:20 +0900

    feat(tests): add Jest configuration and setup for testing with @testing-library/jest-dom

--- jest.config.ts
+++ jest.config.ts
@@ -1,24 +1,19 @@
+import type { Config } from 'jest'
+
 const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 })
-
-// Add any custom config to be passed to Jest
-const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+const customJestConfig: Config = {
+  setupFilesAfterEnv: ['<rootDir>/jest/setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    '/.next/',
-    '/coverage/',
-  ],
+  coveragePathIgnorePatterns: ['/node_modules/', '/.next/', '/coverage/'],
   collectCoverageFrom: [
     'src/**/*.{js,jsx,ts,tsx}',
     '!src/**/*.d.ts',
@@ -34,5 +29,4 @@ const customJestConfig = {
   ],
 }
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig)
