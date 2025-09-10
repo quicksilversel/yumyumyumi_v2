fadf4af30cf5073a33584a20d0a34e08f8dad5aa
quicksilversel <quicksilversel@gmail.com>
2025-09-10 19:08:42 +0900

    fix

--- src/components/pages/Recipe/Recipe.tsx
+++ src/components/pages/Recipe/Recipe.tsx
@@ -14,7 +14,6 @@ export const RecipeDetail = (props: { recipe: Recipe }) => {
   return (
     <RecipeContainer>
       <Header {...props} />
-
       <InnerContainer>
         <Ingredients {...props} />
         <Directions {...props} />
